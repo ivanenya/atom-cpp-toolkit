34dbcc3dd95d0ed02af5bb46ba43ae16cac6c12d
ivanenya <sirius.alexander@gmail.com>
2018-07-12 14:43:47 +0800

    init

---
+++ spec/cpp-toolkit-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CppToolkitView from '../lib/cpp-toolkit-view';
+
+describe('CppToolkitView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
